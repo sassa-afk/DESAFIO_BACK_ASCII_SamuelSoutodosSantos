338f325f3db3f0de9738525d03b87441bd6d1ffc
sassa <samuelsouto21@gmail.com>
2025-10-23 20:09:37 -0300

    --

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,7 +1,7 @@
 import { Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize('process.env.dba', 'process.env.user', 'process.env.pass', {
-  host: 'process.env.host',
+export const sequelize = new Sequelize(process.env.dba, process.env.user, process.env.pass, {
+  host: process.env.host,
   port: 5432,
   dialect: 'postgres',
   dialectOptions: {
