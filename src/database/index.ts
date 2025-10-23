724751ff9e321cb827c9d903293459f3478a1f20
sassa <samuelsouto21@gmail.com>
2025-10-23 20:12:59 -0300

    --

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,11 +1,19 @@
 import { Sequelize } from 'sequelize';
 
-export const sequelize = new Sequelize(process.env.dba, process.env.user, process.env.pass, {
-  host: process.env.host,
-  port: 5432,
-  dialect: 'postgres',
-  dialectOptions: {
-    ssl: { require: true,rejectUnauthorized: false // importante para evitar erro de certificado autoassinado}
-  },
-  logging: false,
-});
+export const sequelize = new Sequelize(
+  process.env.dba as string,
+  process.env.user as string,
+  process.env.pass as string,
+  {
+    host: process.env.host,
+    port: 5432,
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false, // importante para evitar erro de certificado autoassinado
+      },
+    },
+    logging: false,
+  }
+);
